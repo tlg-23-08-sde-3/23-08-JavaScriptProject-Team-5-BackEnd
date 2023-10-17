7865bf07f9fb750303f9eaa718edf54695ea64b1
Jojo George Mattam <jojogeorge.mattam@tlgcohort.com>
2023-10-17 09:11:34 -0500

    Changes to align with client code

--- index.js
+++ index.js
@@ -25,23 +25,19 @@ app.post("/create", async (req, res) => {
       params: {
         key: pixabayApiKey,
         q: prompt,
-        min_width: 150, 
-        min_height: 100, 
-        safesearch: true, 
-        editors_choice: true, 
-        orientation: "horizontal", 
-    }
+        // orientation: "horizontal",
+      },
     });
 
     if (pixabayResponse.data && pixabayResponse.data.hits && pixabayResponse.data.hits.length > 0) {
-      const randomIndex = Math.floor(Math.random() * pixabayResponse.data.hits.length);
-      const randomImageUrl = pixabayResponse.data.hits[randomIndex].previewURL;
-      res.send(randomImageUrl);
+        const randomIndex = Math.floor(Math.random() * pixabayResponse.data.hits.length);
+        const randomImageUrl = pixabayResponse.data.hits[randomIndex].previewURL;
+        res.json({ imageUrl: randomImageUrl });
     } else {
-      res.status(500).send("No images found.");
+        res.status(500).send("No images found.");
     }
-  } catch (err) {
+} catch (err) {
     console.error(err);
     res.status(500).send("An error occurred while fetching the image.");
-  }
+}
 });
